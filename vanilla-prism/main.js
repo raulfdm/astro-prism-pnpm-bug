f246740e077cb08434258a8e0068b0e32b00275a
Raul de Melo <melo.raulf@gmail.com>
2023-03-02 08:48:25 +0100

    remove files and render prism code

--- vanilla-prism/main.js
+++ vanilla-prism/main.js
@@ -1,25 +1,17 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
 import PrismJs from "prismjs";
 
-console.log(PrismJs);
+const code = `const foo = 'bar';`;
+
+const html = PrismJs.highlight(
+  code,
+  PrismJs.languages.javascript,
+  "javascript"
+);
+
 document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1>PrismJS</h1>
+    ${html}
   </div>
 `;
-
-setupCounter(document.querySelector("#counter"));
